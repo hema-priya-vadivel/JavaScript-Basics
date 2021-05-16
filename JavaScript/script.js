8f64ecbeda131794ea2c474d04ce035e61ca46be
TheProgrammer565 <76843640+TheProgrammer565@users.noreply.github.com>
2021-05-16 11:54:54 +0530

    Add files via upload
    Basics of JavaScript

---
+++ JavaScript/script.js
@@ -0,0 +1,28 @@
+alert("Hi,There!");
+
+/*This is an example of
+multi line comment*/
+
+var myApp="Am I a joke to you?";
+prompt(myApp);
+
+var myCal;
+alert(myCal);//value is not defined so it outputs as "undefined"
+
+if(myApp == "Am I a joke to you?"){
+  alert("true!");
+}
+else{
+  alert("false!");
+}
+
+function sum(a,b){
+  alert(a+b);
+}
+sum(2,3)
+
+function myClick(){
+  document.getElementById("para").innerHTML="Learn JavaScipt!Become a SuperHero";
+}
+
+console.log("John"+" "+"is a Doctor.")//Concatenation
