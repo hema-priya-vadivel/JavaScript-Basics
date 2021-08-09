260d120285ae1b88b7298202be2dd400d4d94063
TheProgrammer565 <hemapriyavadivel25@gmail.com>
2021-08-09 11:58:46 +0530

    Add files via upload

---
+++ BasicsOfJS/app.js
@@ -0,0 +1,53 @@
+console.log("hello world");
+console.log("5");
+
+var js="hello world is the example for all programming languages";
+  console.log(js);
+  var us="hema";
+  console.log(us);
+  var hs="priya";
+  console.log(hs);
+  console.log(us+' '+hs);
+  
+console.log("Coding is very useful skil and used to solve many complex probleml");
+console.log("JavaScript,along with HTML and CSS are important for webdevelopers to learn");
+console.log("JavaScript is the best coding language in the world");
+
+var hobby1="sleeping";
+var hobby2="cooking";
+var hobby3="studying";
+console.log("My favorite hobbies are"+" "+hobby1+","+hobby2+","+hobby3+"!");
+var con="computer science";
+console.log(typeof con);
+var yearofbirth=2002;
+  var futureyear=2025;
+    console.log(futureyear-yearofbirth);
+    var futureage=23;
+    console.log(futureage);
+      
+var tomweight=75.5;
+var tomheight=1.74;
+BMIoftom=tomweight/(tomheight*tomheight);
+console.log(BMIoftom);
+var adamweight=81.2;
+var adamheight=1.88;
+BMIofadam=adamweight/(adamheight*adamheight);
+console.log(BMIofadam);
+
+
+if(BMIoftom>BMIofadam){
+  console.log("BMI of tom is greater");
+}
+else{
+  console.log("BMI of adam is greater");
+}
+
+function BMI(weight,height){
+  console.log(weight/(height*height));
+}
+BMI(75.5,1.74);
+BMI(81.2,1.88);
+
+    
+    
+    
